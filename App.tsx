acbeec4604596fae17d5c8a300e84988ae892bec
Jayaprakash B <jayaprakashb@Jayaprakashs-MacBook-Air.local>
2023-11-20 23:53:04 +0530

    Added android lunch icon and splache screen page

--- App.tsx
+++ App.tsx
@@ -1,9 +1,15 @@
-import { View, Text, SafeAreaView, ScrollView } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, ScrollView, Platform } from 'react-native'
+import React, { useEffect } from 'react'
 import FlatCards from './components/FlatCards'
 import ElevatedCards from './components/ElevatedCards'
+import SplashScreen from 'react-native-splash-screen'
 
 const App = () => {
+
+  useEffect(()=>{
+    if(Platform.OS ==="android")
+    SplashScreen.hide();
+  },[])
   return (
     <SafeAreaView>
       <ScrollView>
